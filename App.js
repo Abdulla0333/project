ab060814d329ab6423936fcfcc48838da9c1ac9c
Abdulla0333 <110036692+Abdulla0333@users.noreply.github.com>
2023-09-17 08:59:56 +0500

    custom dropdown

--- App.js
+++ App.js
@@ -1,57 +1,106 @@
-import React, {useState, useRef} from 'react';
-import {View} from 'react-native';
-import MultiSelect from 'react-native-multiple-select';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Modal,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+} from 'react-native';
 
-const items = [
-  {id: '92iijs7yta', name: 'Ondo'},
-  {id: 'a0s0a8ssbsd', name: 'Ogun'},
-  {id: '16hbajsabsd', name: 'Calabar'},
-  {id: 'nahs75a5sg', name: 'Lagos'},
-  {id: '667atsas', name: 'Maiduguri'},
-  {id: 'hsyasajs', name: 'Anambra'},
-  {id: 'djsjudksjd', name: 'Benue'},
-  {id: 'sdhyaysdj', name: 'Kaduna'},
-  {id: 'suudydjsjd', name: 'Abuja'},
+const options = [
+  {id: 1, label: 'Maiduguri'},
+  {id: 2, label: 'Lagos'},
+  {id: 3, label: 'Ogun'},
+  {id: 4, label: 'Abuja'},
+  {id: 5, label: '5Ondo'},
 ];
+console.log(options);
 
 const App = () => {
-  const [selectedItems, setSelectedItems] = useState([]);
-  const multiSelectRef = useRef(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedOptions, setSelectedOptions] = useState([]);
 
-  const onSelectedItemsChange = selectedItems => {
-    setSelectedItems(selectedItems);
+  const toggleOption = option => {
+    const isSelected = selectedOptions.includes(option.label);
+    if (isSelected) {
+      setSelectedOptions(
+        selectedOptions.filter(label => label !== option.label),
+      );
+    } else {
+      setSelectedOptions([...selectedOptions, option.label]);
+    }
   };
 
   return (
-    <View style={{flex: 1, marginHorizontal: 10, backgroundColor: 'white'}}>
-      <MultiSelect
-        hideTags
-        items={items}
-        uniqueKey="id"
-        ref={multiSelectRef}
-        onSelectedItemsChange={onSelectedItemsChange}
-        selectedItems={selectedItems}
-        selectText="Pick Items"
-        searchInputPlaceholderText="Search Items..."
-        onChangeInput={text => console.log(text)}
-        altFontFamily="ProximaNova-Light"
-        tagRemoveIconColor="#000000"
-        tagBorderColor="#000000"
-        tagTextColor="#000000"
-        selectedItemTextColor="#000000"
-        selectedItemIconColor="#000000"
-        itemTextColor="#000000"
-        displayKey="name"
-        searchInputStyle={{color: '#000000'}}
-        submitButtonColor="#000000"
-        submitButtonText="Submit"
-      />
-      <View>
-        {multiSelectRef.current &&
-          multiSelectRef.current.getSelectedItemsExt(selectedItems)}
-      </View>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => setModalVisible(true)}>
+        <Text style={styles.sowDropDown}>Show Dropdown</Text>
+      </TouchableOpacity>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}>
+        <View style={styles.modal}>
+          <FlatList
+            data={options}
+            keyExtractor={item => {
+              return item.id.toString();
+            }}
+            renderItem={({item}) => (
+              <TouchableOpacity
+                style={styles.option}
+                onPress={() => toggleOption(item)}>
+                <Text
+                  style={
+                    selectedOptions.includes(item.label) &&
+                    styles.selectedOption
+                  }>
+                  {item.label}
+                </Text>
+              </TouchableOpacity>
+            )}
+          />
+
+          <TouchableOpacity onPress={() => setModalVisible(false)}>
+            <Text style={styles.sowDropDown}>Done</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+
+      <Text style={{...styles.sowDropDown, width: '80%'}}>
+        Selected Options: {selectedOptions.join(', ')}
+      </Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  modal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  option: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  selectedOption: {
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  sowDropDown: {
+    color: 'black',
+  },
+});
 
 export default App;
